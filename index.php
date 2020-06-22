48a7a8db4309aa78aefbd7c5a345647e3bd849da
Khushi Tulsiyan <khushitulsiyan495@gmail.com>
2020-06-23 00:16:19 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,185 @@
+                        <!DOCTYPE html>
+                        <html>
+                        <head>
+                            <title>Central Perk</title>
+
+
+                             <meta charset="utf-8">
+
+                          <meta name="viewport" content="width=device-width, initial-scale=1">
+
+                            <link rel="stylesheet" type="text/css" href="style.css">
+
+                            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+                            <link href="https://fonts.googleapis.com/css2?family=Caveat&display=swap" rel="stylesheet">
+
+                            </head>
+                        <body>
+
+
+
+                        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                          <a class="navbar-brand" href="#">Central Perk</a>
+                          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                          </button>
+
+                          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ml-auto">
+                              <li class="nav-item active">
+                                <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                              </li>
+
+                                <li class="nav-item">
+                                <a class="nav-link" href="about.php">Services</a>2
+                              </li>
+
+
+                              <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Contact Us</a>
+                              </li>
+
+
+                            </ul>
+                            <form class="form-inline my-2 my-lg-0">
+                              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                            </form>
+                          </div>
+                        </nav>
+
+
+
+
+                            <div id="demo" class="carousel slide" data-ride="carousel">
+
+
+                          <ul class="carousel-indicators">
+                            <li data-target="#demo" data-slide-to="0" class="active"></li>
+                            <li data-target="#demo" data-slide-to="1"></li>
+                            <li data-target="#demo" data-slide-to="2"></li>
+                          </ul>
+
+
+                          <div class="carousel-inner">
+                            <div class="carousel-item active">
+                              <img src="images/coffee.jpg" alt="Cappucino is a saviour" width="1100" height="500">
+                            </div>
+
+                            <div class="carousel-item">
+                              <img src="images/coffee-mug-1493946797-1538791870.jpg" alt="The Ultimate Pleasure" width="1100" height="500">
+                            </div>
+
+
+                            <div class="carousel-item">
+                              <img src="images/coffee2.png" alt="Addicted!" width="1100" height="500">
+                            </div>
+
+                          </div>
+
+
+                          <a class="carousel-control-prev" href="style.css" data-slide="prev">
+                            <span class="carousel-control-prev-icon"></span>
+                          </a>
+                          <a class="carousel-control-next" href="style.css" data-slide="next">
+                            <span class="carousel-control-next-icon"></span>
+                          </a>
+                        </div>
+
+
+                            <section class="my-5">
+                            <div class="py-5">
+
+                                <h2 class="text-center">ABOUT US</h2>
+                                <div class="container-fluid">
+                                <div class="row">
+                                <div class=" col-lg-6 col-md-6 col-12">
+                                <img src="images/Central_Perk.jpg" class="img-fluid" aboutimg>
+                                    </div>    
+
+                                 <div class=" col-lg-6 col-md-6 col-12">
+                                <h2 class="display-4">Hi, We make coffee!</h2>
+                                     <p class="py-5">We are the reason for Manhattan's 90% caffeine addiction;)
+                                     The official caterers for the T.V. show F.R.I.E.N.D.S. It is situated in New York City's Greenwich Village, in the same apartment block as Monica's apartment. It was where they spent much of their free time conversing. There are 97 steps away from Chandler and Joey's apartment as Joey counts in The One With George Stephanopoulos.</p>
+
+                                    <a href="about.php" class="btn btn-success"> Know Us More!</a>
+
+                                    </div>
+
+                                </div>
+
+
+
+                                </div>
+                            </section>
+
+
+                                     <section class="my-5">
+                                <div class="py-5">
+
+                                    <h2 class="text-center">SERVICES!</h2>
+                                         </div>
+
+                                         <div class="container-fluid">
+                                             <div class="row">
+                                                 <div class="col-lg-4 col-md-4 col-12">
+
+
+                                                     <div class="card">
+                  <img class="card-img-top" src="images/coffee.jpg" alt="Card image">
+                  <div class="card-body">
+                    <h4 class="card-title">Coffee</h4>
+                    <p class="card-text">Have some caffeine shots.</p>
+                    <a href="#" class="btn btn-primary">See Menu</a>
+                  </div>
+                                                     </div>
+                                                 </div>
+
+                                                     <div class="col-lg-4 col-md-4 col-12">
+                                                         <div class="card" >
+                  <img class="card-img-top" src="images/tea.webp" alt="Card image">
+                  <div class="card-body">
+                    <h4 class="card-title">Tea</h4>
+                    <p class="card-text">There is no drink as refreshing as tea.</p>
+                    <a href="#" class="btn btn-primary">See Menu</a>
+                  </div>
+                                                         </div>
+                                                 </div>
+
+
+
+                                                              <div class="col-lg-4 col-md-4 col-12">
+                                                             <div class="card">
+                  <img class="card-img-top" src="images/muffin4.jpg" alt="Card image">
+                  <div class="card-body">
+                    <h4 class="card-title">Breads</h4>
+                    <p class="card-text">You don't need any occasion for sweets.</p>
+                    <a href="#" class="btn btn-primary">See Menu</a>
+                  </div>
+                </div>
+
+
+
+
+
+
+                                                 </div>
+                                             </div>
+                                         </div>
+
+
+                                    </section>
+
+
+
+
+
+
+                            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+                        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+                        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+                            </body>
+
+                        </html>
